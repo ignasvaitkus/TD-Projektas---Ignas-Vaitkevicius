c0885897ccc8c09e2769c610691c4456c391d586
ignasvait <vaitkusignas@gmail.com>
2020-02-20 21:02:58 +0200

    Visual Studio projektas pridetas

---
+++ TowerDefense Projektas/TowerDefense Projektas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TowerDefense_Projektas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
